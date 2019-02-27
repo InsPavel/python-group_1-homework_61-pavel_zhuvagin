671555e8f878e15f36cdce983bf80aea9bfc1cbc
pavel <zhpv07@gmail.com>
2019-02-27 13:01:49 -0500

    Подключен reacstrap и bootstrap-grid

--- kona-bikes/src/index.js
+++ kona-bikes/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-4-grid';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
