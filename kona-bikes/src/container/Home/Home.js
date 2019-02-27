855c250fc5fb38a7cad532fdd87ea50aebd58dbe
pavel <zhpv07@gmail.com>
2019-02-27 12:28:01 -0500

    Добавлен компонент Home(главная страница). Подключены стили.

---
+++ kona-bikes/src/container/Home/Home.js
@@ -0,0 +1,16 @@
+import React, {Component, Fragment} from 'react';
+import './Home.css'
+
+class Home extends Component {
+    render() {
+        return (
+            <Fragment>
+                <div className="home">
+                    
+                </div>
+            </Fragment>
+        )
+    }
+}
+
+export default Home;
