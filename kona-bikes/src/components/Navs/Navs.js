0264bc1eb776beeb90d6c575c1f3d4974c42c7e7
pavel <zhpv07@gmail.com>
2019-02-27 21:35:22 -0500

    Немного изменены стили Navs

--- kona-bikes/src/components/Navs/Navs.js
+++ kona-bikes/src/components/Navs/Navs.js
@@ -1,21 +1,21 @@
 import React, {Component, Fragment} from 'react';
 import { Nav, NavItem, NavLink } from 'reactstrap';
 import './Navs.css'
-import logo from '../../images/logos-kona.png'
+import logo from '../../images/kona (1).png'
 
 class Navs extends Component {
     render() {
         return (
             <Fragment>
-                <Nav>
+                <Nav className='nav'>
                     <NavItem>
                         <NavLink onClick={this.props.homeHandler}><img className='logo' src={logo} alt='logo'/></NavLink>
                     </NavItem>
                     <NavItem>
-                        <NavLink onClick={this.props.aboutHandler}>About as</NavLink>
+                        <NavLink className='nav_li' onClick={this.props.aboutHandler}><span>About as</span></NavLink>
                     </NavItem>
                     <NavItem>
-                        <NavLink onClick={this.props.contactsHandler}>Contacts</NavLink>
+                        <NavLink className='nav_li' onClick={this.props.contactsHandler}><span>Contacts</span></NavLink>
                     </NavItem>
                 </Nav>
             </Fragment>
